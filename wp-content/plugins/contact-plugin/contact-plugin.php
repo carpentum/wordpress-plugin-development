7bcbc2a42af661aeff360a4bc62a75411a2f9237
Ramón <ramon.mosquera@gmail.com>
2024-03-12 14:06:19 +0100

    Folder structure, securing plugin, create plugin class, carbon fields install

---
+++ wp-content/plugins/contact-plugin/contact-plugin.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * 
+ * Plugin name: Contact plugin
+ * Description: This is my test plugin
+ * Version: 1.0.0
+ * Text domain: options-plugin
+ * 
+ */
+
+if (!defined('ABSPATH')) die('You cannot be here');
+
+if (!class_exists('ContactPlugin')) {
+
+    class ContactPlugin
+    {
+        public function __construct()
+        {
+            define('MY_PLUGIN_PATH', plugin_dir_path(__FILE__));
+
+            require_once MY_PLUGIN_PATH . 'vendor/autoload.php';
+        }
+    }
+
+    new ContactPlugin;
+}
