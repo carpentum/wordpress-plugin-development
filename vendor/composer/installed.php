787b4ab19fb1142dd9ef20f9b47d54f9e905cdd5
Ramón <ramon.mosquera@gmail.com>
2024-03-13 09:26:10 +0100

    Setting up carbon fields for our custom plugin options

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -19,14 +19,5 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
-        'htmlburger/carbon-fields' => array(
-            'pretty_version' => 'v3.6.3',
-            'version' => '3.6.3.0',
-            'reference' => 'd913a5148cb9dc61ed239719c747f4ebb513003f',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
     ),
 );
--- wp-content/plugins/contact-plugin/includes/options-page.php
+++ wp-content/plugins/contact-plugin/includes/options-page.php
@@ -1,1 +1,26 @@
 <?php
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action('carbon_fields_register_fields', 'create_options_page');
+function create_options_page()
+{
+    Container::make('theme_options', __('Contact Form'))
+        ->add_fields([
+            Field::make('checkbox', 'contact_plugin_active', __('Active')),
+            Field::make('text', 'contact_plugin_reciepients', __('Recipient email'))
+                ->set_attribute('placeholder', 'e.g.email@email.com')
+                ->set_help_text('The email that the form is submitted to'),
+            Field::make('textarea', 'contact_plugin_message', __('Confirmation message'))
+                ->set_attribute('placeholder', 'Enter confirmation message')
+                ->set_help_text('Type the message you want the submitter to receive')
+
+        ]);
+}
+
+add_action('after_setup_theme', 'load_carbon_fields');
+function load_carbon_fields()
+{
+    \Carbon_Fields\Carbon_Fields::boot();
+}
