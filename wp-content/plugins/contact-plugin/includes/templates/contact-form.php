3a7edf4477b1ac73304a7c27a44530f890aee93d
Ramón <ramon.mosquera@gmail.com>
2024-03-13 09:53:07 +0100

    Create form template for front end display

--- wp-content/plugins/contact-plugin/includes/templates/contact-form.php
+++ wp-content/plugins/contact-plugin/includes/templates/contact-form.php
@@ -1,1 +1,15 @@
-dgsdfgsfgs
+<form>
+    <label for="name">Name</label><br>
+    <input type="text" name="name"><br><br>
+
+    <label for="email">Email</label><br>
+    <input type="text" name="email"><br><br>
+
+    <label for="phone">Phone</label><br>
+    <input type="text" name="phone"><br><br>
+
+    <label for="message">Your message</label><br>
+    <textarea name="message" id="message"></textarea><br><br>
+
+    <button type="submit">Submit form</button>
+</form>
