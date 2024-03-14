9d67f6a6e18cdc350dcf69182cdf15f493d96fd3
Ramón <ramon.mosquera@gmail.com>
2024-03-14 20:41:53 +0100

    Handle form data (Nonce check, send email)

--- wp-content/plugins/contact-plugin/includes/contact-form.php
+++ wp-content/plugins/contact-plugin/includes/contact-form.php
@@ -4,7 +4,11 @@ add_action('rest_api_init', 'create_rest_endpoint');
 
 function show_contact_form()
 {
-    include MY_PLUGIN_PATH . '/includes/templates/contact-form.php';
+    ob_start();
+    load_template(MY_PLUGIN_PATH . '/includes/templates/contact-form.php');
+    $ret = ob_get_contents();
+    ob_end_clean();
+    return $ret;
 }
 
 function create_rest_endpoint()
@@ -15,7 +19,36 @@ function create_rest_endpoint()
     ]);
 }
 
-function handle_enquiry()
+function handle_enquiry($data)
 {
-    echo 'hello';
+    $params = $data->get_params();
+
+    if (!wp_verify_nonce($params['_wpnonce'], 'wp_rest')) {
+        return new WP_REST_Response('There has been an error sending the email!', 422);
+    }
+
+    unset($params['_wpnonce']);
+    unset($params['_wp_http_referer']);
+
+    // Send the email message
+    $headers = [];
+
+    $admin_email = get_bloginfo('admin_email');
+    $admin_name = get_bloginfo('name');
+
+    $headers[] = "From: {$admin_name} <{$admin_email}>";
+    $headers[] = "Reply-to: {$params['name']} <{$params['email']}>";
+    $headers[] = "Content-type: text/html";
+
+    $subject = "New enquiry from {$params['name']}";
+    $message = '';
+    $message .= "<h1>Message has been sent from {$params['name']}</h1><br><br>";
+
+    foreach ($params as $label => $value) {
+        $message .= '<strong>' . ucfirst($label) . "</strong>: " . $value . '<br>';
+    }
+
+    wp_mail($admin_email, $subject, $message, $headers);
+
+    return new WP_REST_Response('The message was sent successfully!', 200);
 }
--- wp-content/plugins/contact-plugin/includes/templates/contact-form.php
+++ wp-content/plugins/contact-plugin/includes/templates/contact-form.php
@@ -1,4 +1,10 @@
+<div id="form_success" style="background:green;color:white"></div>
+<div id="form_error" style="background:red;color:white"></div>
+
 <form id="enquiry_form">
+
+    <?php wp_nonce_field('wp_rest'); ?>
+
     <label for="name">Name</label><br>
     <input type="text" name="name"><br><br>
 
@@ -13,3 +19,26 @@
 
     <button type="submit">Submit form</button>
 </form>
+
+<script>
+    $(document).ready(function() {
+        $('#enquiry_form').submit(function(event) {
+            event.preventDefault();
+
+            var form = $(this);
+
+            $.ajax({
+                type: "POST",
+                url: "<?php echo get_rest_url(null, 'v1/contact-form/submit'); ?>",
+                data: form.serialize(),
+                success: function(res) {
+                    form.hide();
+                    $("#form_success").html(res).fadeIn();
+                },
+                error: function(res) {
+                    $("#form_error").html(res.responseText.replace(/\"/g, "")).fadeIn();
+                }
+            })
+        })
+    })
+</script>
