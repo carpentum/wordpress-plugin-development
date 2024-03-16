7b431a6922b8c0efb48a72609ac8b6853f92ab21
Ramón <ramon.mosquera@gmail.com>
2024-03-16 15:21:01 +0100

    Creating custom columns on submission post type

--- wp-content/plugins/contact-plugin/includes/contact-form.php
+++ wp-content/plugins/contact-plugin/includes/contact-form.php
@@ -3,6 +3,38 @@ add_shortcode('contact', 'show_contact_form');
 add_action('rest_api_init', 'create_rest_endpoint');
 add_action('init', 'create_submissions_page');
 add_action('add_meta_boxes', 'create_meta_box');
+add_action('manage_submission_posts_columns', 'custom_submission_columns');
+add_action('manage_submission_posts_custom_column', 'fill_submission_columns', 10, 2);
+
+function fill_submission_columns($column, $post_id)
+{
+    switch ($column) {
+        case 'name':
+            echo get_post_meta($post_id, 'name', true);
+            break;
+        case 'email':
+            echo get_post_meta($post_id, 'email', true);
+            break;
+        case 'phone':
+            echo get_post_meta($post_id, 'phone', true);
+            break;
+        case 'message':
+            echo get_post_meta($post_id, 'message', true);
+            break;
+    }
+}
+
+function custom_submission_columns($columns)
+{
+    $columns = [
+        'cb' => $columns['cb'],
+        'name' => __('Name', 'contact-plugin'),
+        'email' => __('Email', 'contact-plugin'),
+        'phone' => __('phone', 'contact-plugin'),
+        'message' => __('message', 'contact-plugin'),
+    ];
+    return $columns;
+}
 
 function create_meta_box()
 {
